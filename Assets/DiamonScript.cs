015c8a55329c3fa03815bcf8778ffc7e731e4576
alexander gustafsson <alexander.gustafsson@elev.ga.lbs.se>
2018-10-02 10:47:46 +0200

    La till en "bil"

--- Assets/DiamonScript.cs
+++ Assets/DiamonScript.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DiamonScript : MonoBehaviour {
+public class DiamonScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    public SpriteRenderer rend;
+    public Color newColor;
+    public Transform other;
+    [Range(-720, 720)]
+    public float rotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend.color = newColor;
+        rend.color = new Color(1f, 0.5f, 0.3f);
+       //other.position = new Vector3(3, 2, other.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
+        }
+       if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(1000f * Time.deltaTime, 0, 0);
+        }
+       if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-1000f * Time.deltaTime, 0, 0);
+        }
+    }
 }
